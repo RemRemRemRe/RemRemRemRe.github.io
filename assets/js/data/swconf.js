6e82575530245c6a1e64321ba0078c666ae9544a
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-02-15 01:27:32 +0800

    Site update No.33

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771089722',resources: [
+    cacheName: 'chirpy-1771090038',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
